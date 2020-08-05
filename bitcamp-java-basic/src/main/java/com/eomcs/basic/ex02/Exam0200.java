4027ce06eeb76c20f45ce24b86349d7b5108a320
rngus4274 <35832052+rngus4274@users.noreply.github.com>
2020-08-06 05:13:53 +0900

    수정

--- bitcamp-java-basic/src/main/java/com/eomcs/basic/ex02/Exam0200.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex02/Exam0200.java
@@ -1,42 +1,45 @@
-// Exam0200.java
-// 주제 : javadoc 주석
-
 package com.eomcs.basic.ex02;
 
+//# 주석 - Javadoc 주석
+//
+//- "java document comment(doc comment)"
+//- javadoc에서 HTML 문서를 만들 때 사용하는 주석이다.
+//- 주로 API 문서를 자동 생성할 때 사용한다.
+//- 클래스나 메서드, 변수 선언에 붙일 수 있다.
+//
+
 /**
- * 클래스에 대한 설명 클래스에 대한 설명
- * <p>
- * 중간에 HTML 태그를 넣을 수 있다
- * </p>
  * 클래스에 대한 설명
- * 
- * @author 홍길동
+ * @author eomjinyoung
+ *
  */
 public class Exam0200 {
-    /**
-     * 필드에 대한 설명<br>
-     * <b>필드</b>에 대한 설명<br>
-     */
-    int a;
-
-    /**
-     * 메서드에 대한 설명<br>
-     * <i>메서드</i>에 대한 설명<br>
-     * 메서드에 대한 설명
-     * 
-     * @param args 파라미터에 대한 설명
-     * @param args 파라미터에 대한 설명
-     * @param args 파라미터에 대한 설명
-     */
-    public static void main(String[] args) {
-
-    }
-
-    /**
-     * 메서드 설명<br>
-     * 메서드 설명...ok!
-     */
-    public void m() {
-        // ...
-    }
+  /**
+   * 변수에 대한 설명
+   * 변수 선언 앞에 설명을 붙여 놓으면 나중에 HTML 문서를 만들 때 추출할 수 있다.
+   */
+  static String message = "Hello, world!";
+  
+  /**
+   * 메서드에 대한 설명
+   * 메서드에 대한 설명을 여기에 붙여 놓으면 나중에 HTML 문서를 만들 때 추출할 수 있다.
+   * @param args 애플리테이션 아규먼트 값을 보관한 배열
+   */
+  public static void main(String[] args) {
+    System.out.println(message);
+  }
 }
+
+//## 실습
+//1) Java Document 만들기
+//- javadoc.exe 도구를 사용하여 자바 소스 파일에서 Javadoc 주석을 추출하여 HTML 파일을 생성해 보자.
+//     javadoc 
+//       -encoding [소스 파일의 문자집합]
+//       -charset [생성될 HTML 파일의 문자집합]
+//       -d [생성된 파일을 놓아둘 디렉토리] 
+//       -sourcepath [자바 소스 경로] [자바 패키지]
+//예) $ javadoc -encoding UTF-8 -charset UTF-8 -d javadoc -sourcepath src/main/java com.eomcs.basic.ex02
+//
+//2) Javadoc으로 생성한 HTML 문서 확인하기
+//- /javadoc 디렉토리를 보면 자바 소스 파일에서 추출한 Javadoc 으로 만든 HTML 문서를 확인할 수 있다.
+// 
--- bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0100.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/basic/ex03/Exam0100.java
@@ -6,39 +6,26 @@ package com.eomcs.basic.ex03;
 
 public class Exam0100 {
   public static void main(String[] args) {
-    // 출력 명령문
-    // - System.out.println(값);
-
-    // [숫자]
-    // 정수
-    System.out.println(78);
+    System.out.println("-------- 정수 리터럴");
+    System.out.println(78); // 10진수
     System.out.println(+78);
     System.out.println(-78);
-    System.out.println(0116); // 0으로 시작하면 8진수
-    System.out.println(0x4e); // 0x42 0x로 시작하면 16진수
-    System.out.println(0b01001110); // 0b01001110 0b로 시작하면 2진수
-
-    // 부동소수점(실수)
+    System.out.println(0116); // 8진수
+    System.out.println(0x4e); // 16진수
+    System.out.println(0b01001110); // 2진수
+    
+    System.out.println("-------- 부동소수점 리터럴");
     System.out.println(3.14);
     System.out.println(31.4e-1);
-    System.out.println(314e-2);
-    System.out.println(0.0314e2);
-
-    // [논리]
-    // - 참/거짓
+    
+    System.out.println("-------- 논리 리터럴");
     System.out.println(true);
     System.out.println(false);
-
-    // [문자]
-    // 문자
-    System.out.println('A');
+    
+    System.out.println("-------- 문자 리터럴");
     System.out.println('가');
-
-    // 문자열
-    System.out.println("A");
-    System.out.println("가");
-    System.out.println("");
-    System.out.println("ABC");
-    System.out.println("가나다");
+    
+    System.out.println("-------- 문자열 리터럴");
+    System.out.println("오호라 코딩스쿨!");
   }
 }
